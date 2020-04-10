b88866d020341ab34b53cd309594fc1e09ef2c04
Jesse Langley <jlangleyiii@aol.com>
2020-04-10 04:50:52 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -7,16 +7,16 @@ def my_hash_creator(key, value)
 end
 
 def read_from_hash(hash, key)
-  if hash[key]
 hash[key]
-end
+
   # return the correct value using the hash and key parameters
 end
 
 def update_counting_hash(hash, key)
   if hash[key]
     hash[key] += 1
-  else hash[key] = 1
+  else 
+    hash[key] = 1
 end  
  end
  
